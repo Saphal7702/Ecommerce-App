1030cd7d2697cd35addcdbca5f5cb53d81965e93
Saphal7702 <saphal7702@gmail.com>
2022-04-04 02:58:29 -0600

    Added CRUD for product and categories

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ require("dotenv").config();
 //Configuring user routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+
 
 const app = express();
 
@@ -23,6 +26,8 @@ app.use(expressValidator());
 //Using routes middleware
 app.use("/api",authRoutes);
 app.use("/api",userRoutes);
+app.use("/api",categoryRoutes);
+app.use("/api",productRoutes);
 
 const port = process.env.PORT || 8000;
 
